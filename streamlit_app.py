5b42c6577eb07c7c747c9efac4577f1356cc0f72
Shubham Dhawan CL <shubham.dhawan@igtsolutions.com>
2023-05-02 21:48:46 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,7 @@ if uploaded_file is not None:
     # st.write(string_data)
 
     # # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    # dataframe = pd.read_csv(uploaded_file)
+    # st.write(dataframe)
+    st.write(uploaded_file.name)
+    st.write(type(uploaded_file.name))
