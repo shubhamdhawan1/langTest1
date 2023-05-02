5792eb5b0c727a8d8f9ac726cd756bae910e4031
Shubham Dhawan CL <shubham.dhawan@igtsolutions.com>
2023-05-02 19:28:05 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,13 @@ if uploaded_file is not None:
     # st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     # st.write(stringio)
 
     # # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+    # string_data = stringio.read()
+    # st.write(string_data)
 
     # # Can be used wherever a "file-like" object is accepted:
-    # dataframe = pd.read_csv(uploaded_file)
-    # st.write(dataframe)
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
