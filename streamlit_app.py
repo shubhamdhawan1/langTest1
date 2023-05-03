cbf1f91e75c7ea6d7a6c34f91f3609220663139c
Shubham Dhawan CL <shubham.dhawan@igtsolutions.com>
2023-05-03 10:00:45 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,20 @@ st.title("Test 1")
 st.subheader("Lets Input Data")
 # st.date_input()
 
+import csv
 import pandas as pd
 from io import StringIO
+from langchain.document_loaders.csv_loader import CSVLoader
 
-def csv_reader(file):
-    dataframe = pd.read_csv(file)
-    st.write(dataframe)
+# def csv_reader(file):
+#     dataframe = pd.read_csv(file)
+#     st.write(dataframe)
+
+def csv_reader(file, delimiter = ',', quotechar = '"', escapechar = None, doublequote = True,skipinitialspace = False, lineterminator = '\r\n', quoting = 'QUOTE_MINIMAL'):
+    # loader = CSVLoader(file_path='./example_data/mlb_teams_2012.csv')
+    loader = CSVLoader(file_path=file)
+    data = loader.load()
+    st.write(data)
 
 
 uploaded_file = st.file_uploader("Choose a file")
@@ -32,8 +40,7 @@ if uploaded_file is not None:
     # st.write(uploaded_file.name.split('.')[1])
     # st.write(type(uploaded_file.name))
     if(uploaded_file.name.split('.')[1]=='csv'):
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+        csv_reader(uploaded_file)
     if(uploaded_file.name.split('.')[1]=='txt'):
         bytes_data = uploaded_file.getvalue()
         st.write(bytes_data)
